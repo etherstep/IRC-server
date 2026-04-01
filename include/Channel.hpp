d4efe06e26daced621e215bb7729c51425b30101
mursupaani <antti.pollanen89@gmail.com>
2026-04-01 14:55:28 +0300

    Implementing flags

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <stdexcept>
 #include <vector>
 
 // a - toggle the anonymous channel flag;
@@ -11,20 +12,6 @@
 // s - toggle the secret channel                             flag;
 // r - toggle the server reop channel                        flag;
 // t - toggle the topic settable by channel operator only flag;
-enum class ChannelFlag : uint16_t {
-  NONE = 0,
-  ANONYMOUS = 1,
-  INVITE_ONLY = 1 << 1,
-  MODERATED = 1 << 2,
-  NO_MESSAGES_FROM_OUTSIDE = 1 << 3,
-  QUIT_CHANNEL = 1 << 4,
-  PRIVATE_CHANNEL = 1 << 5,
-  SECRET_CHANNEL = 1 << 6,
-  SERVER_REOP_CHANNEL = 1 << 7,
-  TOPIC_SET_MY_CHANOP_ONLY = 1 << 8
-
-};
-
 class Channel {
   private:
     uint16_t _channelFlags = 0;
@@ -34,6 +21,21 @@ class Channel {
     Channel(const Channel &other) = delete;
     Channel &operator=(const Channel &other) = delete;
     ~Channel();
+
+    enum class ChannelFlag : uint16_t {
+      ANONYMOUS = 1,
+      INVITE_ONLY = 1 << 1,
+      MODERATED = 1 << 2,
+      NO_MESSAGES_FROM_OUTSIDE = 1 << 3,
+      QUIT_CHANNEL = 1 << 4,
+      PRIVATE_CHANNEL = 1 << 5,
+      SECRET_CHANNEL = 1 << 6,
+      SERVER_REOP_CHANNEL = 1 << 7,
+      TOPIC_SET_MY_CHANOP_ONLY = 1 << 8
+    };
+
+    void toggleChannelFlag(const ChannelFlag flag);
+    void resetChannelFlags(void);
 
     // O - give "channel creator" status;
     void giveChannelCreatorStatus(void);
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,1 +1,36 @@
 #include "Channel.hpp"
+
+Channel::Channel() {}
+
+Channel::~Channel() {}
+
+void Channel::resetChannelFlags(void) {
+  _channelFlags = 0;
+}
+
+void Channel::toggleChannelFlag(const ChannelFlag flag) {
+  switch (flag) {
+    case ChannelFlag::ANONYMOUS:
+      _channelFlags ^= (1 << static_cast<uint16_t>(flag));
+      break;
+    case ChannelFlag::INVITE_ONLY:
+      break;
+    case ChannelFlag::MODERATED:
+      break;
+    case ChannelFlag::NO_MESSAGES_FROM_OUTSIDE:
+      break;
+    case ChannelFlag::QUIT_CHANNEL:
+      break;
+    case ChannelFlag::PRIVATE_CHANNEL:
+      break;
+    case ChannelFlag::SECRET_CHANNEL:
+      break;
+    case ChannelFlag::SERVER_REOP_CHANNEL:
+      break;
+    case ChannelFlag::TOPIC_SET_MY_CHANOP_ONLY:
+      break;
+    default:
+      throw std::runtime_error("Unidentified flag");
+      break;
+  }
+}
